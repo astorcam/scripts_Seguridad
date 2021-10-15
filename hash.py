230eef55d1cfbf30e43c38a7bf57f9aaf484b7e4
astorcam <62352203+astorcam@users.noreply.github.com>
2021-10-15 09:08:24 +0200

    Update hash.py

--- hash.py
+++ hash.py
@@ -3,10 +3,10 @@ import subprocess
 hashOriginal=input("Inserta el hash verdadero:")
 pwd=os.getcwd()
 print(pwd)
-for f in os.listdir("imagenes"):
-    output= subprocess.Popen(["md5sum " + pwd + "/imagenes/"+f], stdout=subprocess.PIPE, shell=True)
+for f in os.listdir("imagenes"): #debes escribir el nombre de la carpeta de los archivos a analizar
+    output= subprocess.Popen(["md5sum " + pwd + "/imagenes/"+f], stdout=subprocess.PIPE, shell=True) #debes cambiar el directorio donde estan los archivos
     output=output.stdout.read().decode("utf-8")
     hash=output.split()
     if hashOriginal == (hash[0]):
-            print ("La imagen que buscas es " + f)
+            print ("El archivo que buscas es " + f)
             break
